14b06708d7cae100404be9e97e2af6769fd785e1
AstariTTos <63050335+AstariTTos@users.noreply.github.com>
2022-05-01 20:24:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -30,7 +30,7 @@ bool search(std::string s, char z) {
 }
 
 std::string infx2pstfx(std::string inf) {
-   TStack<char, 100> stack;
+  TStack<char, 100> stack;
   std::stack<char> Stack;
   std::string number = "0123456789";
   std::string operation = "()+-*/";
